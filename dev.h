83a6629a2601441423d6a8f16925fe5e3aa6236a
Steve.Braneky <stephen.braneky@lairdtech.com>
2017-07-28 16:25:17 -0400

    Laird add support for channel 165
    Bug:11703
    (cherry picked from commit 60f7976dc383a2fd26ad71358f4ce60c22c8677f)

--- dev.h
+++ dev.h
@@ -126,7 +126,7 @@
 /* Band related constants */
 #define BAND_24_CHANNEL_NUM                 14
 #define BAND_24_RATE_NUM                    13
-#define BAND_50_CHANNEL_NUM                 24
+#define BAND_50_CHANNEL_NUM                 25
 #define BAND_50_RATE_NUM                    8
 
 /* vif and station */
