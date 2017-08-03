d2f3538e200fb9d0f249687711bb3a42efa9b148
Ramesh Radhakrishnan <rramesh@marvell.com>
2017-08-03 15:43:37 +0530

    in-STA scan changes - Stop BA inactivity timer during PreScan & restart it in PostScan handler
    (cherry picked from commit 8290785e04ab73b8121466c75795eb2370b32027)
    (cherry picked from commit 6f69eb791558f558371efb1240035082804336ed)

--- main.h
+++ main.h
@@ -7,6 +7,6 @@ extern int wmm_turbo;
 int mwl_add_card(void *, struct mwl_if_ops *);
 void mwl_wl_deinit(struct mwl_priv *);
 void mwl_set_caps(struct mwl_priv *priv);
-
+extern void timer_routine(unsigned long data);
 
 #endif
