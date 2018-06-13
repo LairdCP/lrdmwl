0d76ae9b7e4358ed2e7021a6bf0597b453728282
Steve.Braneky <stephen.braneky@lairdtech.com>
2018-06-13 13:10:11 -0400

    Laird: Start DeepSleep timer after init.
    Also reverted enabling interrupts until after fw is ready for reason given in
    original comment.
    
    (cherry picked from commit 7932ec252d213375b3333f08ed010bb32272a996)
    (cherry picked from commit 5d158aecb6dc2399826d92002bfb2ba64e9ef6d1)
    (cherry picked from commit c50a3afd595717b79ae025bde1f670a32b58d708)

--- main.c
+++ main.c
@@ -985,7 +985,6 @@ int mwl_add_card(void *card, struct mwl_if_ops *if_ops)
 	           priv->ds_enable == DS_ENABLE_ON ? "enabled": "disabled");
 
 	timer_setup(&priv->ds_timer, ds_routine, 0);
-	mwl_restart_ds_timer(priv, true);
 
 	rc = mwl_wl_init(priv);
 	if (rc) {
@@ -1000,6 +999,11 @@ int mwl_add_card(void *card, struct mwl_if_ops *if_ops)
 
 	wiphy_info(priv->hw->wiphy, "%d TX antennas, %d RX antennas\n",
 		   priv->ant_tx_num, priv->ant_rx_num);
+
+	/* Start DS timer after init where radio is registered with mac802.11
+	 * and IEEE80211_CONF_IDLE flag is setup
+	 */
+	mwl_restart_ds_timer(priv, true);
 
 #ifdef CONFIG_SYSFS
 	lrd_sysfs_init(hw);
