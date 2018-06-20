3f52dc10e7de0061364756312626a03b2d544aa8
accessrr <accessramesh@gmail.com>
2018-06-20 15:03:57 +0530

    [RK]Changing driver version P29=>P30 for release
    (cherry picked from commit 38b9323a0f8c7e8b77412da8be022b08d363a5bc)

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P29-20180515"
+#define MWL_DRV_VERSION	 "P30-20180620"
 
 #define LRD_DESC         "Laird 60 Series Wireless Network Driver"
 #define LRD_AUTHOR       "Laird Technologies"
