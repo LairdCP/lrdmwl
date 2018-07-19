9513aaac018d59479b1eaefd4e2f603efe5c76f6
Service DevOps Buildserver <service.devops.buildserver@lairdtech.com>
2018-07-19 15:13:23 +0000

    bump to build number 5.0.0.30

--- dev.h
+++ dev.h
@@ -33,7 +33,7 @@
 #define MWL_DRV_VERSION	 "P22.2-20180509"
 #define LRD_DESC         "Laird 60 Series Wireless Network Driver"
 #define LRD_AUTHOR       "Laird Technologies"
-#define LRD_BLD_VERSION  "5.0.0.29"
+#define LRD_BLD_VERSION  "5.0.0.30"
 #define LRD_DRV_VERSION   LRD_BLD_VERSION "-" MWL_DRV_VERSION
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
