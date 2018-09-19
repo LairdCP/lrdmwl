90f4077f5a103bcdb941c97aebcd2664a1c74d1a
Sharvari Harisangam <Sharvari@marvell.com>
2018-09-19 16:22:09 +0530

    Changing driver version P33=>P34 for release
    (cherry picked from commit 181a390d446bd806fc723d1c01c2edaf4e789a43)

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P33-20180809"
+#define MWL_DRV_VERSION	 "P34-20180919"
 
 #define LRD_DESC         "Laird 60 Series Wireless Network Driver"
 #define LRD_AUTHOR       "Laird Technologies"
