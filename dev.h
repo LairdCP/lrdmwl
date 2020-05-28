dda4df07434baaa907d397a9d9e04a0451e307aa
Service DevOps SCM <service.devops.scm@lairdconnect.com>
2020-05-28 17:45:21 +0000

    bump to build number 7.2.0.5

--- dev.h
+++ dev.h
@@ -37,7 +37,7 @@
 
 #define LRD_DESC         "Laird Connectivity 60 Series Wireless Network Driver"
 #define LRD_AUTHOR       "Laird Connectivity"
-#define LRD_BLD_VERSION  "7.2.0.4"
+#define LRD_BLD_VERSION  "7.2.0.5"
 #define LRD_DRV_VERSION   LRD_BLD_VERSION "-" MWL_DRV_VERSION
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
